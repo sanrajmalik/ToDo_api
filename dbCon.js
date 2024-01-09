8ac57886dafa76b0e3d689ed98be340ef41a0955
Sanraj Malik <sanrajmalik@gmail.com>
2024-01-09 21:51:45 +0530

    change mongouri

--- dbCon.js
+++ dbCon.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config()
 mongoose.set("strictQuery",true);
 
 const DBConnect = async ()=>{
     try{
-        const connect = await mongoose.connect("mongodb+srv://sanraj:sanraj@cluster0.zayt9uv.mongodb.net/ToDoApp");
+        const connect = await mongoose.connect(process.env.MONGO_URI);
         console.log("DB connected", connect.connection.host)
     }catch(e){
         console.log("error"+ e)
